b56b70b308fa099741d08a28f5c08b7e6d997d7a
brayn003 <rudraprasaddas@outlook.com>
2016-04-03 01:58:06 +0530

    added ui-router

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-angular.module('zaya', ['ui.bootstrap']);
+var app = angular.module('zaya', ['ui.bootstrap', 'ui.router','ngAnimate']);
+app.config(function($stateProvider, $urlRouterProvider){
+	// For any unmatched url, send to /route1
+	$urlRouterProvider.otherwise("/create")
+
+	$stateProvider
+	.state('home', {
+		url: "/",
+		templateUrl: "index.html"
+	})
+	.state('create', {
+		url: "/create",
+        views: {
+            "view-c-container": {
+				templateUrl: "create.html",
+            },
+            "view-c-lesson": {
+				templateUrl: "create.lesson.html",
+            },
+            "view-c-topic": {
+				templateUrl: "create.topic.html",
+            }
+        }
+	})
+	// .state('create.topic', {
+	// 	url: "/topic",
+	// 	templateUrl: "create.topic.html"
+	// })
+	// .state('create.course', {
+	// 	url: "/course",
+	// 	templateUrl: "create.course.html"
+	// })
+	.state('dash', {
+		url: "/dash",
+		templateUrl: "dash.html"
+	})
+	.state('reports', {
+		url: "/reports",
+		templateUrl: "reports.html"
+	})
+})
+app.controller('HomeController', function ($scope) {
+  $scope.name = 'Superman';
+  $scope.isCollapsed = true;
+});
