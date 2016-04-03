334ecb6c52825d8a485c1c87c4a00702a01bf6ff
brayn003 <rudraprasaddas@outlook.com>
2016-04-04 03:47:48 +0530

    added draggability resolved data changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-var app = angular.module('zaya', ['ui.bootstrap', 'ui.router','ngAnimate']);
+var app = angular.module('zaya', ['ui.bootstrap', 'ui.router','ngAnimate','dndLists']);
 app.config(function($stateProvider, $urlRouterProvider){
 	// For any unmatched url, send to /route1
 	$urlRouterProvider.otherwise("/create")
 
 	$stateProvider
-	// .state('home', {
-	// 	url: "/",
-	// 	templateUrl: "home.html"
-	// })
 	.state('create', {
 		url: "/create",
         views: {
@@ -20,17 +16,10 @@ app.config(function($stateProvider, $urlRouterProvider){
             },
             "view-c-course@create": {
 				templateUrl: "create.course.html",
+				controller: "ControllerCTopic"
             }
         }
 	})
-	// .state('create.topic', {
-	// 	url: "/topic",
-	// 	templateUrl: "create.topic.html"
-	// })
-	// .state('create.course', {
-	// 	url: "/course",
-	// 	templateUrl: "create.course.html"
-	// })
 	.state('dash', {
 		url: "/dash",
 		templateUrl: "dash.html"
@@ -44,7 +33,8 @@ app.controller('HomeController', function ($scope) {
 	$scope.name = 'Superman';
 	$scope.isCollapsed = true;
 }).controller('ControllerCLesson', function ($scope, $http){
-	$scope.hello = "boobs";
+	// $scope.hello = "boobs";
+	$scope.lessons = [];
 	$http({
         method : "POST",
         url : "data/lessons.json"
@@ -54,4 +44,25 @@ app.controller('HomeController', function ($scope) {
     }, function myError(response) {
         alert("Can't do it");
     });
+
+    $scope.lessons.selected = null;
+
+}).controller('ControllerCTopic', function ($scope){
+	$scope.courses = [];
+	$scope.courses.push({"id" : 1, "name" : "", "lessons" : []});
+
+	$scope.addCourse = function(){
+		$scope.courses.push({"id" : $scope.courses[$scope.courses.length-1].id+1, "name" : "", "lessons" : []});
+	}
+
+	$scope.checkDuplicate = function(param,paramarr){
+		if(paramarr.indexOf(param) != -1){
+			return false;
+		}else{
+			return true;
+		}
+	};
+	// $scope.checkEmpty = function(){
+	// 	$scope.courses.splice($index, 1);
+	// };
 });
