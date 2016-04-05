82973fdd7c925fd1e54332052f19a4316016f8b2
brayn003 <rudraprasaddas@outlook.com>
2016-04-05 05:40:14 +0530

    added filters added search added profile pic resolved bugs

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Zaya UI
+This repository holds the codebase for the assignment provided by Zaya. 
+
+##Instructions
+Instuctions for building the UI is given in `instruction/` folder
+
+##Tasks completed
+- Lesson data generates dynamocally from JSON.
+- Filter parameters work properly.
+- Lessons can be dragged and then dropped at the topic section.
+- Topics can be added on the fly.
+
+
+## Important Notes
+- Links, except for `create`, don't have any functionality on navbar as not instruction was not provided for the same.
+- The `cancel` and `create` button doesn't have any functionality, as not instruction was not provided for the same.
+- Tweaked the funtionality of filter bar to better suit needs.
+- The lesson data is dyanamic and is fetched from `data/lessons.json` using ajax ($http in angular).
+- The site is not static and should be loaded on `localhost` to run properly.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('zaya', ['ui.bootstrap', 'ui.router','ngAnimate','dndLists']);
+var app = angular.module('zaya', ['ui.bootstrap', 'ui.router','ngAnimate','angular.filter','dndLists','ui.select']);
 app.config(function($stateProvider, $urlRouterProvider){
 	// For any unmatched url, send to /route1
 	$urlRouterProvider.otherwise("/create")
@@ -34,18 +34,55 @@ app.controller('HomeController', function ($scope) {
 	$scope.isCollapsed = true;
 }).controller('ControllerCLesson', function ($scope, $http){
 	// $scope.hello = "boobs";
-	$scope.lessons = [];
+	$scope.lessons = {
+		"data" : [],
+		"selected" : null
+	};
+	$scope.filters = {
+		"subjects" : [],
+		"grades" : [],
+		"search" : null
+	};
+	$scope.options = {
+		"subjects" : [],
+		"grades" : []
+	};
+
 	$http({
         method : "POST",
         url : "data/lessons.json"
     }).then(function mySucces(response) {
-    	// alert(JSON.stringify(response));
-        $scope.lessons = response.data;
+        $scope.lessons.data = response.data;
+        for (var i = 0; i < $scope.lessons.data.length; i++) {
+        	if($scope.options.grades.indexOf($scope.lessons.data[i].grade) == -1)
+				$scope.options.grades.push($scope.lessons.data[i].grade);
+		}
+        for (var i = 0; i < $scope.lessons.data.length; i++) {
+        	if($scope.options.subjects.indexOf($scope.lessons.data[i].subject) == -1)
+				$scope.options.subjects.push($scope.lessons.data[i].subject);
+		}
+
     }, function myError(response) {
         alert("Can't do it");
     });
 
-    $scope.lessons.selected = null;
+    $scope.gradeFilter = function(elem){
+    	if ($scope.filters.grades.length == 0)
+    		return true;
+    	return $scope.filters.grades.indexOf(elem.grade) != -1;
+    };
+    $scope.subjectFilter = function(elem){
+    	if ($scope.filters.subjects.length == 0)
+    		return true;
+    	return $scope.filters.subjects.indexOf(elem.subject) != -1;
+    };
+    // $scope.filters.grades.push(1);
+    // alert($scope.gradeFilter());
+
+    // $scope.lessons.selected = null;
+
+	// 
+	// }
 
 }).controller('ControllerCTopic', function ($scope){
 	$scope.courses = [];
